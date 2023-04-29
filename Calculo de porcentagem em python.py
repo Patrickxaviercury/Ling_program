47aaae8eb3a418146f8193e1a92ae14de03dfbd2
Patrick Xavier <123217203+Patrickxaviercury@users.noreply.github.com>
2023-04-29 18:22:11 -0300

    Update Calculo de porcentagem em python.py

--- Calculo de porcentagem em python.py
+++ Calculo de porcentagem em python.py
@@ -3,5 +3,5 @@ qtd = int(input('Quantidade: '))
 total = valor * qtd
 desconto = total * 0.1
 total_final = total - desconto
-print(f'total com desconto: R$ {total_final:.2f}')
+print(f'total com desconto: R$ {total_final:.2f}') #f .2f {} simbolo monetario
 
